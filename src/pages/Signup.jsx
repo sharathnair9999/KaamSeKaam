5cbfa6c8555739a69a034dd9bed99c375d73bc12
Sharath <sharathnair9999@gmail.com>
2022-05-14 13:14:19 +0530

    Signup functionality

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,27 +1,35 @@
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
-  signOut,
   updateProfile,
 } from "firebase/auth";
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { useAuth } from "../contexts/user-context/user-context";
-import { constants } from "../helpers";
+import { constants, onChange } from "../helpers";
+import { InputField, SelectAvatar } from "../components";
+import "./styles/Auth.css";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  const { maleAvatar, femaleAvatar } = constants;
-  const [credentials, setCredentials] = useState({
-    email: "test@test2.com",
-    password: "testuser2",
+  const navigate = useNavigate();
+  const initialCredentialState = {
+    email: "",
+    password: "",
     displayName: "",
     photoURL: "",
-  });
+  };
+  const [credentials, setCredentials] = useState(initialCredentialState);
 
   const { auth } = useAuth();
 
-  const signUpUser = (email, password, displayName, photoURL) => {
-    createUserWithEmailAndPassword(auth, email, password)
+  const signUpUser = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      credentials.email,
+      credentials.password
+    )
       .then(
         ({
           user: {
@@ -33,10 +41,14 @@ const Signup = () => {
       )
       .then((user) =>
         updateProfile(user, {
-          displayName,
-          photoURL,
+          displayName: credentials.displayName,
+          photoURL: credentials.photoURL,
         })
       )
+      .then(() => {
+        toast.success("Welcome!");
+        setCredentials(initialCredentialState);
+      })
       .catch((error) => {
         console.log(error.message);
       });
@@ -56,7 +68,73 @@ const Signup = () => {
 
   return (
     <div>
-    Signup
+      <section className="container-header">
+        <section className="flex-and-center gap-sm font-3xl">
+          <h1>Sign Up</h1>
+          <span className="app-name">{` | Kaam Se Kaam`}</span>
+        </section>
+      </section>
+      <div className="container">
+        <div className="login-container flex-and-center gap-1">
+          <img
+            src={constants.imgUrls.auth}
+            alt="auth"
+            className="responsive-img auth-img"
+          />
+          <form onSubmit={signUpUser} className="fields">
+            <InputField
+              legend={"Name"}
+              type="text"
+              name="displayName"
+              value={credentials.displayName}
+              autoFocus
+              required
+              onChange={(e) => onChange(e, setCredentials)}
+            />
+            <InputField
+              legend={"Email"}
+              type="email"
+              name="email"
+              value={credentials.email}
+              required
+              onChange={(e) => onChange(e, setCredentials)}
+            />
+            <InputField
+              legend={"Password"}
+              type="password"
+              name="password"
+              required
+              value={credentials.password}
+              onChange={(e) => onChange(e, setCredentials)}
+            />
+            <section className=" flex-and-center flex-col gap-sm">
+              Select Avatar
+              <section className="flex-and-center gap-2">
+                <SelectAvatar
+                  credentials={credentials}
+                  setCredentials={setCredentials}
+                  id="male-avatar"
+                  thisAvatar={constants.imgUrls.maleAvatar}
+                />
+                <SelectAvatar
+                  credentials={credentials}
+                  setCredentials={setCredentials}
+                  id="female-avatar"
+                  thisAvatar={constants.imgUrls.femaleAvatar}
+                />
+              </section>
+            </section>
+            <button type="submit">Sign Up</button>
+            <p>Existing User?</p>
+            <button
+              onClick={() => navigate("/login")}
+              className="btn btn-primary"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
