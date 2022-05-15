3a789ed73b15d542c7dd9e4dcf1fa3148206597d
Sharath <sharathnair9999@gmail.com>
2022-05-15 20:23:07 +0530

    Task Container Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-import TaskModal from "./TaskModal/TaskModal";
 import Timer from "./Timer/Timer";
 import InputField from "./InputField/InputField";
 import SelectAvatar from "./InputField/SelectAvatar";
+import TaskContainer from "./TaskComponents/TaskContainer";
 
-export { TaskModal, Timer, InputField, SelectAvatar };
+export { Timer, InputField, SelectAvatar, TaskContainer };
