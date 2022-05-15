0c905eadd7b9f9afc4766b8cf6ab3060d1e1d0d7
Sharath <sharathnair9999@gmail.com>
2022-05-15 20:22:44 +0530

    services seggregated

--- src/services/index.js
+++ src/services/index.js
@@ -1,8 +1,19 @@
+import { logoutUser, signInUser, hiddenSignIn } from "./auth-services";
 import {
   getUserTasks,
+  addTask,
+  deleteTask,
+  updateTask,
+  pinTaskHandler,
+} from "./task-services";
+
+export {
   logoutUser,
   signInUser,
   hiddenSignIn,
-} from "./task-services";
-
-export { getUserTasks, logoutUser, signInUser, hiddenSignIn };
+  getUserTasks,
+  addTask,
+  deleteTask,
+  updateTask,
+  pinTaskHandler,
+};
--- src/services/task-services.js
+++ src/services/task-services.js
@@ -1,96 +1,111 @@
 import {
-  signInAnonymously,
-  signInWithEmailAndPassword,
-  signOut,
-} from "firebase/auth";
-import { collection, getDocs } from "firebase/firestore";
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  setDoc,
+} from "firebase/firestore";
 import { toast } from "react-toastify";
-import { db, auth } from "../firebase/firebase-config";
-import { constants } from "../helpers";
+import { db } from "../firebase/firebase-config";
 
-export const signInUser = async (e, setLoader, setUser, credentials) => {
-  e.preventDefault();
-  setLoader(true);
+export const getUserTasks = (userId, dispatch) => {
   try {
-    const {
-      user: { displayName, photoURL, email, uid },
-    } = await signInWithEmailAndPassword(
-      auth,
-      credentials.email,
-      credentials.password
-    );
-    setUser((state) => ({
-      ...state,
-      isLoggedIn: true,
-      name: displayName,
-      photoURL,
-      email,
-      uid,
-    }));
-    localStorage.setItem(
-      "userDetails",
-      JSON.stringify({
-        name: displayName,
-        photoURL,
-        email,
-        isLoggedIn: true,
-        uid,
-      })
-    );
-    setLoader(false);
-  } catch (error) {
-    setLoader(false);
-    toast.error(error.message);
-  }
-};
+    console.log("getting");
+    dispatch({ type: "LOADING_TASKS", payload: true });
+    const querySnapshot = query(collection(db, `userData/${userId}/tasks`));
 
-export const hiddenSignIn = async (setUser) => {
-  try {
-    const {
-      user: { uid },
-    } = await signInAnonymously(auth);
-    setUser((state) => ({
-      ...state,
-      isLoggedIn: true,
-      name: "Anonymous User",
-      photoURL: constants.imgUrls.maleAvatar,
-      uid,
-    }));
-    localStorage.setItem(
-      "userDetails",
-      JSON.stringify({
-        name: "Anonymous User",
-        photoURL: constants.imgUrls.maleAvatar,
-        isLoggedIn: true,
-        uid,
-      })
-    );
-    toast.success("Signed In as Anonymous User");
-  } catch (error) {
-    toast.error(error.message);
-  }
-};
-
-export const logoutUser = async (setter) => {
-  await signOut(auth);
-  localStorage.removeItem("userDetails");
-  setter({
-    isLoggedIn: false,
-    name: "",
-    email: "",
-    uid: "",
-    photoURL: "",
-  });
-};
-export const getUserTasks = async (userId) => {
-  try {
-    const querySnapshot = await getDocs(
-      collection(db, `userData/${userId}/tasks`)
-    );
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
+    const unsubscribe = onSnapshot(querySnapshot, (snapshot) => {
+      const tasks = [];
+      snapshot.forEach((doc) => {
+        tasks.push({
+          taskId: doc.id,
+          ...doc.data(),
+        });
+        const completedTasks = tasks.filter((task) => task.isCompleted);
+        const pendingTasks = tasks.filter((task) => !task.isCompleted);
+        dispatch({ type: "GET_PENDING_TASKS", payload: pendingTasks });
+        dispatch({ type: "GET_COMPLETED_TASKS", payload: completedTasks });
+      });
     });
-  } catch (err) {
-    console.log(err);
+    dispatch({ type: "LOADING_TASKS", payload: false });
+    return unsubscribe;
+  } catch (error) {
+    dispatch({ type: "LOADING_TASKS", payload: false });
+    toast.error("Could not retrieve your Tasks. Please try later!");
+  }
+};
+
+export const addTask = async (userId, currTaskState) => {
+  try {
+    await addDoc(collection(db, `userData/${userId}/tasks`), {
+      taskName: currTaskState.taskName,
+      taskDescription: currTaskState.taskDescription,
+      taskDuration: Number(currTaskState.taskDuration),
+      shortBreak: Number(currTaskState.shortBreak),
+      longBreak: Number(currTaskState.longBreak),
+      isCompleted: Boolean(false),
+      isPinned: Boolean(currTaskState.isPinned),
+    });
+    toast.success(`New Task Added Successfully`);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+};
+
+export const deleteTask = async (userId, taskId, dispatch) => {
+  try {
+    const collectionRef = collection(db, `userData/${userId}/tasks`);
+    const docRef = doc(collectionRef, taskId);
+    await deleteDoc(docRef);
+    dispatch({ type: "DELETE_PENDING_TASK", payload: taskId });
+    toast.success(`Deleted Task Successfully`);
+  } catch (error) {
+    toast.error(error);
+  }
+};
+
+export const updateTask = async (userId, task) => {
+  try {
+    const collectionRef = collection(db, `userData/${userId}/tasks`);
+    const docRef = doc(collectionRef, task.taskId);
+    setDoc(docRef, {
+      taskName: task.taskName,
+      taskDescription: task.taskDescription,
+      taskDuration: task.taskDuration,
+      longBreak: task.longBreak,
+      shortBreak: task.shortBreak,
+      isCompleted: false,
+      isPinned: task.isPinned,
+    });
+    toast.success("Updated Task Successfully");
+  } catch (error) {
+    toast.error(error);
+  }
+};
+
+export const pinTaskHandler = async (userId, task) => {
+  try {
+    const collectionRef = collection(db, `userData/${userId}/tasks`);
+    const docRef = doc(collectionRef, task.taskId);
+    setDoc(docRef, {
+      taskName: task.taskName,
+      taskDescription: task.taskDescription,
+      taskDuration: task.taskDuration,
+      longBreak: task.longBreak,
+      shortBreak: task.shortBreak,
+      isCompleted: false,
+      isPinned: !task.isPinned,
+    });
+    toast.success(
+      `${
+        task.isPinned
+          ? "Removed Pin from the Task"
+          : "Pinned the task to stay on top"
+      }`
+    );
+  } catch (error) {
+    toast.error(error);
   }
 };
